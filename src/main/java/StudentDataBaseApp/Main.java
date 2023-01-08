738a851f9c68b03ca0d0545a8ea42ef29949c6b2
Danila Aldanov <danilaaldanov73@gmail.com>
2023-01-08 13:01:12 +0300

    added a constructor of Student class

--- src/main/java/StudentDataBaseApp/Main.java
+++ src/main/java/StudentDataBaseApp/Main.java
@@ -1,4 +1,9 @@
 package StudentDataBaseApp;
 
 public class Main {
+    public static void main(String[] args) {
+        //Ask how mane new users we want to add
+        Student st1 = new Student();
+        //Create n number of new students
+    }
 }
--- src/main/java/StudentDataBaseApp/Student.java
+++ src/main/java/StudentDataBaseApp/Student.java
@@ -1,6 +1,8 @@
 package StudentDataBaseApp;
 
-public class DataBaseApp {
+import java.util.Scanner;
+
+public class Student {
     private String firstName;
     private String lastName;
     private int gradeYear;
@@ -11,7 +13,19 @@ public class DataBaseApp {
 
 
     // Constructor prompted user to enter student's name and year
+    public Student () {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter student first name: ");
+        this.firstName = sc.nextLine();
+
+        System.out.print("Enter student last name: ");
+        this.lastName = sc.nextLine();
+
+        System.out.print("1 - Freshman\n2 - Sophomore\n3 - Junior\n4 - Senior\nEnter student class level: ");
+        this.gradeYear = sc.nextInt();
+        System.out.println(firstName + " " + lastName + " " + gradeYear);
+    }
     //Generate an ID
 
     //Enroll in courses
