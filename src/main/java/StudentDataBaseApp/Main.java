0400fb791c77ac0c9e3c7119db63c201664cceec
Danila Aldanov <danilaaldanov73@gmail.com>
2023-01-10 10:19:51 +0300

    Added an array of students, method that show all information ofcertain student, added a method for paying

--- src/main/java/StudentDataBaseApp/Main.java
+++ src/main/java/StudentDataBaseApp/Main.java
@@ -1,10 +1,32 @@
 package StudentDataBaseApp;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Student st1 = new Student();
-        //Ask how mane new users we want to add
+        //Ask how many students we want to add and create n number of new students
+        createArrayOfStudent(countOfStudents());
+    }
 
-        //Create n number of new students
+    //Create a method to ask how mane new users we want to add
+    static public int countOfStudents() {
+        System.out.print("How many students do you want to add: ");
+        int countOfStudents = new Scanner(System.in).nextInt();
+        return countOfStudents;
+    }
+
+    //Create n number of new students
+    static public void createArrayOfStudent(int countOfStudent) {
+        Student[] student = new Student[countOfStudent];
+        //Add students up into array
+        for (int i = 0; i < countOfStudent; i++) {
+            student[i] = new Student();
+            student[i].enrollInCourse();
+        }
+
+        //Print information of students
+        for (int i = 0; i < countOfStudent; i++) {
+            student[i].showInformationOfAStudent();
+        }
     }
 }
--- src/main/java/StudentDataBaseApp/Student.java
+++ src/main/java/StudentDataBaseApp/Student.java
@@ -3,9 +3,9 @@ package StudentDataBaseApp;
 import java.util.Scanner;
 
 public class Student {
-    private String firstName;
-    private String lastName;
-    private String gradeYear;
+    private final String firstName;
+    private final String lastName;
+    private final String gradeYear;
     private String studentID;
     private String courses = "";
     private int tuitionBalance;
@@ -15,7 +15,7 @@ public class Student {
     private static int ID = 1000;
 
     // Constructor prompted user to enter student's name and year
-    public Student () {
+    public Student() {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter student first name: ");
@@ -29,59 +29,73 @@ public class Student {
 
         //add a studentID to our student
         setStudentID();
-        enrollInCourse();
-        //test
-        System.out.println("Student info.\n" +
-                "Name: " + firstName + " " + lastName + ". Grade year: " + gradeYear +
-                " student ID: " + studentID + "\n" +
-                "List of student's courses:\n" + courses + "Tuition balance: " + tuitionBalance);
     }
+
     //Generate an ID
     public String setStudentID() {
         ID++;
         return studentID = gradeYear + ID;
     }
+
     //Enroll in courses
     public void enrollInCourse() {
         int course;
         System.out.println("What course do student attend");
         do {
-            System.out.println("""
-                    1 - History 101
-                    2 - English 101
-                    3 - Math 101
-                    4 - PE 101
-                    0 - Quit
+            System.out.print("""
+                    1 - History 101;
+                    2 - English 101;
+                    3 - Math 101;
+                    4 - PE 101;
+                    0 - Quit;
                     Enter a number of course:\s""");
             Scanner sc = new Scanner(System.in);
             course = sc.nextInt();
             switch (course) {
                 case (1) -> {
-                    this.courses = this.courses + "History 101\n";
+                    this.courses = this.courses + "\tHistory 101\n";
                     this.tuitionBalance = tuitionBalance + costOfCourse;
                 }
                 case (2) -> {
-                    this.courses = this.courses + "English 101\n";
+                    this.courses = this.courses + "\tEnglish 101\n";
                     this.tuitionBalance = tuitionBalance + costOfCourse;
                 }
                 case (3) -> {
-                    this.courses = this.courses + "Math 101\n";
+                    this.courses = this.courses + "\tMath 101\n";
                     this.tuitionBalance = tuitionBalance + costOfCourse;
                 }
                 case (4) -> {
-                    this.courses = this.courses + "PE 101\n";
+                    this.courses = this.courses + "\tPE 101\n";
                     this.tuitionBalance = tuitionBalance + costOfCourse;
                 }
-                case (0) -> {}
+                case (0) -> {
+                }
                 default -> System.out.println("In developing");
             }
         } while (course != 0);
-
-
     }
+
     //View balance
+    public void showBalance() {
+        System.out.println("Your balance is $" + tuitionBalance);
+    }
 
     //Pay tuition
+    public void payTuitionBalance() {
+        System.out.print("Enter your deposit here: ");
+        int deposit = new Scanner(System.in).nextInt();
+        this.tuitionBalance = tuitionBalance - deposit;
+        //After operation check a tuition balance
+        showBalance();
+    }
 
     //Show an information of a student
+    public void showInformationOfAStudent() {
+        System.out.println("Student info.\n" +
+                "\tName: " + firstName + " " + lastName +
+                ".\n\tGrade year: " + gradeYear +
+                ".\n\tStudent ID: " + studentID + "\n" +
+                "\tList of student's courses:\n" + courses +
+                "\tTuition balance: " + tuitionBalance);
+    }
 }
